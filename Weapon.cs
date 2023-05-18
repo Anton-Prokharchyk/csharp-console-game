f3aacf38efb9a0bd4172c2d0ce3ef204fef25eef
Anton.Prokharchyk <api40411off@gmail.com>
2023-05-18 13:38:09 +0300

    added class weapon and config file

---
+++ Weapon.cs
@@ -0,0 +1,44 @@
+namespace Game
+{
+    class Weapon
+    {
+        private int locationX = 0;
+        private int locationY = 0;
+        public bool isMoving = false;
+        static private string shape = "}";
+        public Weapon(int coordX, int coordY)
+        {
+            locationX = coordX;
+            locationY = coordY;
+        }
+        public int LocationX
+        {
+            get => locationX;
+            set
+            {
+                if (value > 0) locationX = value;
+            }
+        }
+        public int LocationY
+        {
+            get => locationY;
+            set
+            {
+                if (value > 0) locationY = value;
+            }
+        }
+        public string Shape
+        {
+            get => shape;
+            set
+            {
+                shape = value;
+            }
+        }
+        public void Move()
+        {
+            // if (isMoving) locationX++;
+            locationX++;
+        }
+    }
+}
